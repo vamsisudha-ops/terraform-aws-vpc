9f660909310c5c00d757d0b16f4c3921f4cea0bf
vamsisudha-ops <vamsisudha49@gmail.com>
2026-05-09 18:49:05 +0530

    terraform-aws-vpc

--- peering.tf
+++ peering.tf
@@ -1,35 +1,34 @@
-# resource "aws_vpc_peering_connection" "default" {
-#   count = var.is_peering_required ? 1 : 0
-  
-#   peer_vpc_id   = data.aws_vpc.default.id # acceptor
-#   vpc_id        = aws_vpc.main.id
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0
+  peer_vpc_id   = data.aws_vpc.default.id # acceptor
+  vpc_id        = aws_vpc.main.id  # requestor
 
-#   accepter {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   requester {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   auto_accept   = true
+  auto_accept   = true
 
-#   tags = merge(
-#     var.vpc_tags,
-#     local.common_tags,
-#     {
-#         Name = "${local.common_name_suffix}-default"
-#     }
-#   )
+  tags = merge(
+    var.vpc_tags,
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-default"
+    }
+  )
 
-# }
+}
 
-# resource "aws_route" "public_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = aws_route_table.public.id
-#   destination_cidr_block    = data.aws_vpc.default.cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
-# }
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
 
 # resource "aws_route" "private_peering" {
 #   count = var.is_peering_required ? 1 : 0
@@ -38,9 +37,9 @@
 #   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 # }
 
-# resource "aws_route" "default_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = data.aws_route_table.main.id
-#   destination_cidr_block    = var.vpc_cidr
-#   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
-# }
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.main.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
--- variables.tf
+++ variables.tf
@@ -74,7 +74,7 @@ variable "nat_gateway_tags" {
     default = {}
 }
 
-# variable "is_peering_required" {
-#     type = bool
-#     default = true
-# }
+variable "is_peering_required" {
+    type = bool
+    default = true
+}
